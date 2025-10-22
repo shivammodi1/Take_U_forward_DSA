8d024deddd6ae6cf0efeb375474c8f39369020de
Shivam Modi~ <smodi9846@gmail.com>
2025-10-22 19:20:36 +0530

    leetcode 2177

---
+++ Arrays/2177. Find Three Consecutive Integers That Sum to a Given Number.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<long long> sumOfThree(long long num)
+{
+    // num % num == 0 hoga toh hi solution hoga otherwise no solution
+    // let x is first element
+    // { x, x+1, x+2 }
+    // 3x+3 => 3(x+1) = num
+
+    if (num % 3 != 0)
+    {
+        return {};
+    }
+
+    long long x = num / 3 - 1;
+    return {x, x + 1, x + 2};
+}
