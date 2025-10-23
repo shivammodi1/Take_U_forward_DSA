31291d7c35bc6e1595144f8ef0490c6920188330
Shivam Modi~ <smodi9846@gmail.com>
2025-10-23 21:41:52 +0530

    gfg

---
+++ Arrays/Subarrays with sum K.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// method 1
+// T.C. -> O(N^2)
+int cntSubarrays(vector<int> &arr, int k)
+{
+    // code here
+    int count = 0;
+    int sum = 0;
+    int n = arr.size();
+    for (int i = 0; i < n; i++)
+    {
+        sum = 0;
+        for (int j = i; j < n; j++)
+        {
+            sum += arr[j];
+            if (sum == k)
+            {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+// method using map
+// method 2
+// T.C. -> O(n)
+int findAllSubarraysWithGivenSum(vector < int > & arr, int k) {
+    int n = arr.size(); // size of the given array.
+    map mpp;
+    int preSum = 0, cnt = 0;
+
+    mpp[0] = 1; // Setting 0 in the map.
+    for (int i = 0; i < n; i++) {
+        // add current element to prefix Sum:
+        preSum += arr[i];
+
+        // Calculate x-k:
+        int remove = preSum - k;
+
+        // Add the number of subarrays to be removed:
+        cnt += mpp[remove];
+
+        // Update the count of prefix sum
+        // in the map.
+        mpp[preSum] += 1;
+    }
+    return cnt;
+}
+
+int main()
+{
+    vector arr = {3, 1, 2, 4};
+    int k = 6;
+    int cnt = findAllSubarraysWithGivenSum(arr, k);
+    cout << "The number of subarrays is: " << cnt << "\n";
+    return 0;
+}
