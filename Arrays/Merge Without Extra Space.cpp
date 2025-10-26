93dce64ab2ea4c6422154fd195a9a776ae3305d4
Shivam Modi~ <smodi9846@gmail.com>
2025-10-26 21:38:36 +0530

    Merge Without Extra Space

---
+++ Arrays/Merge Without Extra Space.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+// Input: a[] = [2, 4, 7, 10], b[] = [2, 3]
+// Output: a[] = [2, 2, 3, 4], b[] = [7, 10]
+// Explanation: After merging the two non-decreasing arrays, we get, [2, 2, 3, 4, 7, 10]
+
+int main(){
+
+}
