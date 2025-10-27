5718f3a626126e8c29f12fdde016dcf0dbe827be
Shivam Modi~ <smodi9846@gmail.com>
2025-10-27 21:14:33 +0530

    Count Inversion gfgs

--- Arrays/Count Inversions.cpp
+++ Arrays/Count Inversions.cpp
@@ -6,3 +6,25 @@
 // Input: arr[] = [2, 4, 1, 3, 5]
 // Output: 3
 // Explanation: The sequence 2, 4, 1, 3, 5 has three inversions (2, 1), (4, 1), (4, 3).
+#include<bits/stdc++.h>
+using namespace std;
+
+// Method 1
+// T.C -> O(N^2)
+class Solution {
+  public:
+    // Function to count inversions in the array.
+    int inversionCount(vector<int> &arr) {
+        // Your Code Here
+        int N = arr.size();
+        int count = 0;
+    for(int i = 0;i<N;i++){
+        for(int j=i+1;j<N;j++){
+            if(arr[i]>arr[j] && i<j) {
+                count++;
+            }
+        }
+    }
+    return count;
+    }
+};
