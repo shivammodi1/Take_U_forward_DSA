1fdb539ed0eb68876e6bccb50905118bd0f31d30
Shivam Modi~ <smodi9846@gmail.com>
2025-10-29 20:49:46 +0530

    upperBound

--- Binary Search/Implement Upper Bound.cpp
+++ Binary Search/Implement Upper Bound.cpp
@@ -3,9 +3,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int lowerBound(vector<int>&arr,int target){
-    
-}
+class Solution {
+public:
+    int upperBound(vector<int>& arr, int target) {
+        int n = arr.size();
+        int st = 0, end = n - 1, ans = n;
+
+        while (st <= end) {
+            int mid = st + (end - st) / 2;
+
+            if (arr[mid] > target) {
+                ans = mid;
+                end = mid - 1;
+            } else {
+                st = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
 
 int main(){
 
