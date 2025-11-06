d9d45e34f7735a0888188df93d48e2fb96f66852
Shivam Modi~ <smodi9846@gmail.com>
2025-11-06 22:03:35 +0530

    Aggresive cow

--- Binary Search/Aggressive Cows.cpp
+++ Binary Search/Aggressive Cows.cpp
@@ -8,3 +8,54 @@
 // the second cow can be placed at stalls[2] and 
 // the third cow can be placed at stalls[3]. 
 // The minimum distance between cows in this case is 3, which is the largest among all possible ways.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int aggressiveCows(vector<int> &arr, int k) {
+        // Number of stalls
+        int n = arr.size();
+
+        // Sort the stall positions
+        sort(arr.begin(), arr.end());
+        
+        // Search space for minimum distance between cows
+        int st = 0;
+        int end = arr[n-1] - arr[0]; // maximum possible distance between first and last stall
+        int ans;
+
+        // Binary search to find the largest minimum distance
+        while (st <= end) {
+            int mid = st + (end - st) / 2; // mid represents a possible minimum distance
+            
+            int cow = 1; // place the first cow at the first stall
+            int lastPos = arr[0]; // last placed cow position
+
+            // Try placing the remaining cows
+            for (int i = 1; i < n; i++) {
+                // If current stall is at least 'mid' distance away from last placed cow
+                if (arr[i] - lastPos >= mid) {
+                    cow++; // place another cow
+                    lastPos = arr[i]; // update last placed position
+                }
+            }
+            
+            // If we can place all cows with at least 'mid' distance
+            if (cow >= k) {
+                ans = mid;     // store the possible answer
+                st = mid + 1;  // try for a larger distance
+            } else {
+                end = mid - 1; // reduce distance
+            }
+        }
+        
+        return ans; // return the largest minimum distance
+    }
+};
+int aggressiveCows(vector<int> &stalls, int k) {
+    Solution sol;
+    return sol.aggressiveCows(stalls, k);
+}
+
