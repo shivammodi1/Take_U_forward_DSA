b5b3f55a7277f403d86b9d2bca6a4a9427d0da12
Shivam Modi~ <smodi9846@gmail.com>
2025-11-09 12:26:04 +0530

    Reverse Words in a String

---
+++ String/Reverse Words in a String.cpp
@@ -0,0 +1,17 @@
+// Input: s = "the sky is blue"
+// Output: "blue is sky the"
+
+// Input: s = "  hello world  "
+// Output: "world hello"
+// Explanation: Your reversed string should not contain leading or trailing spaces.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+string reverseWords(string s) {
+        
+}
+
+int main(){
+
+}
