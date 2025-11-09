22bde6cf1e32b8a109c87f4cdee008dbbdca16b7
Shivam Modi~ <smodi9846@gmail.com>
2025-11-09 19:23:50 +0530

    Isomorphic String Solution

---
+++ String/Isomorphic Strings.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        // this for storing their index at there ASIIC index
+        vector<int>indexS(256,-1); 
+        vector<int>indexT(256,-1);
+
+        for(int i=0;i<s.size();i++){
+            // at the same place if their value is different than false
+            if(indexS[s[i]] != indexT[t[i]]){
+                return false;
+            }
+            // updating the index 
+            indexS[s[i]]=i;
+            indexT[t[i]]=i;
+        }
+
+        return true;
+    }
+};
+
