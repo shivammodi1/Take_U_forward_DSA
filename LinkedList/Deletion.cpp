47a7d3627e84d3473cd1dd0eadbe09f4c2beb8cf
Shivam Modi~ <smodi9846@gmail.com>
2025-11-19 22:33:01 +0530

    Deletion at last and particular node node

--- LinkedList/Deletion.cpp
+++ LinkedList/Deletion.cpp
@@ -12,6 +12,15 @@ class node{
     }
 };
 
+// if particular node only needs to be deleted
+void deleteNode(node* delNode){
+    node* delNode->data = delNode->next->data;
+    node* toDelete = delNode->next;
+    delNode->next = delNode->next->next;
+    delete toDelete;
+}
+
+
 // Function to delete the first node of the linked list
 node* deleteAtFirst(node* head){
     if(!head){
@@ -29,6 +38,21 @@ node* deleteAtFirst(node* head){
 }
 
 // Function to delete the last node of the linked list
+node* deleteAtEnd(node* head){
+    if(!head){
+        return NULL;
+    }
+    node* tmp=head;
+    // till we reach second last node
+    while(tmp->next->next != NULL){
+        tmp=tmp->next;
+    }
+    // now tmp points to second last node
+    node* toDelete = tmp->next;
+    tmp->next = NULL; // unlink the last node
+    delete toDelete; // free memory
+    return head;
+}
 
 int main(){
 
