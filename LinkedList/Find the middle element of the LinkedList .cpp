21e3290479f73742be06ba617aaa4eca3e9d64c7
Shivam Modi~ <smodi9846@gmail.com>
2025-11-23 14:29:33 +0530

    method 1

--- LinkedList/Find the middle element of the LinkedList .cpp
+++ LinkedList/Find the middle element of the LinkedList .cpp
@@ -12,6 +12,29 @@ class node{
     }
 };
 
+//Method 1
+int getMiddle(node* head){
+    if(!head){
+        return NULL;
+    }
+    // step 1: count number of nodes
+    // step 2: traverse till count/2
+    int count = 0;
+    node* tmp = head;
+    while(tmp){
+        count++;
+        tmp=tmp->next;
+    }
+
+    int mid = count/2;
+    tmp=head;
+    while(mid--){
+        tmp=tmp->next;
+    }
+
+    return tmp->data;
+}
+
 int main(){
 
 }
