a61680a8cb6a24f46fef0dac1a1fc3badc8cc6e6
Shivam Modi~ <smodi9846@gmail.com>
2025-11-23 21:02:06 +0530

    gfg length of loop

--- LinkedList/Length of Loop in Linked List.cpp
+++ LinkedList/Length of Loop in Linked List.cpp
@@ -16,7 +16,28 @@ public:
 
 int lengthOfLoop(Node *head)
 {
-    
+    if(!head){
+        return NULL;
+    }    
+
+    Node* slow = head;
+    Node* fast = head;
+    while(slow && fast && fast->next){
+        slow = slow->next;
+        fast = fast->next->next;
+
+        // loop found
+        if(slow==fast){
+            int count=1;
+            // jab tak fast ka next slow ke barabar na ho count++
+            while(fast->next!=slow){
+                count++;
+                fast=fast->next;
+            }
+            return count;
+        }
+    }
+    return 0;
 }
 
 int main()
