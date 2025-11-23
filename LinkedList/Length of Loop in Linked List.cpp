d94ba1ed78c1a268de44fdd391fe5984aea7d048
Shivam Modi~ <smodi9846@gmail.com>
2025-11-23 20:27:52 +0530

    gfg length of loop

---
+++ LinkedList/Length of Loop in Linked List.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+int lengthOfLoop(Node *head)
+{
+    
+}
+
+int main()
+{
+}
