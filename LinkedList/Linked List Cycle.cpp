9922bbb53b49545eca8257e956fe327a2dd94ee6
Shivam Modi~ <smodi9846@gmail.com>
2025-11-23 15:38:17 +0530

    Method 2 Linked List Cycle

--- LinkedList/Linked List Cycle.cpp
+++ LinkedList/Linked List Cycle.cpp
@@ -1,13 +1,57 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class node{
-    public:
+class node
+{
+public:
     int data;
-    node* next;
+    node *next;
 
-    node(int val){
+    node(int val)
+    {
         data = val;
-        next=NULL;
+        next = NULL;
     }
 };
+
+//method 1 using 2 pointer
+bool detectLoop(node *head)
+{
+    if(!head){
+        return NULL;
+    }
+    node* slow = head;
+    node* fast = head;
+
+    while(slow && fast && fast->next){
+        slow=slow->next;
+        fast=fast->next->next;
+
+        if(slow==fast){
+            return true;
+        }
+    }
+    return false;
+
+}
+
+//method 2 using map
+// In map store node and their bool value present or not
+// if node already present return true 
+// return false
+
+bool detectLoop(node* head){
+    unordered_map<node*,bool> visit;
+    node* tmp = head;
+    while(tmp){
+        // if node already present
+        if(visit[tmp]==1){
+            return true;
+        }
+        // In evry visit make it present at that node
+        visit[tmp] = 1;
+        tmp=tmp->next;
+    }
+    //after traversing there is no loop detected
+    return false;
+}
