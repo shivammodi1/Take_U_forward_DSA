c04df67701db47690c0303e13bc800ce4b4eb91d
Shivam Modi~ <smodi9846@gmail.com>
2025-11-23 14:39:16 +0530

    slow fast pointer approch

--- LinkedList/Find the middle element of the LinkedList .cpp
+++ LinkedList/Find the middle element of the LinkedList .cpp
@@ -35,6 +35,24 @@ int getMiddle(node* head){
     return tmp->data;
 }
 
+
+// Method 2 Slow and fast pointer
+int getMid(node* head){
+    if(!head){
+        return NULL;
+    }
+    node* slow = head; // it will move 1x speed
+    node* fast = head; // it will move 2x speed
+    while(fast && fast->next){
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    // at the end fast is placed at end node 
+    // and slow at mid node
+    return slow->data;
+
+}
+
 int main(){
 
 }
