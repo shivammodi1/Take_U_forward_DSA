c94ab02d84790980b10bb9261cc5e152774ffffa
Shivam Modi~ <smodi9846@gmail.com>
2025-11-24 23:12:36 +0530

    delete middle node

--- LinkedList/Delete Middle of Linked List.cpp
+++ LinkedList/Delete Middle of Linked List.cpp
@@ -1,13 +1,43 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class node{
-    public:
+class Node
+{
+public:
     int data;
-    node* next;
+    Node *next;
 
-    node(int val){
+    Node(int val)
+    {
         data = val;
         next = NULL;
     }
 };
+
+Node *deleteMid(Node *head)
+{
+    if (!head || !head->next)  // single node or empty list
+        return NULL;
+
+    int count = 0;
+    Node* tmp = head;
+
+    while (tmp) {
+        count++;
+        tmp = tmp->next;
+    }
+
+    int mid = count / 2;
+
+    tmp = head;
+    Node* prev = NULL;
+
+    for (int i = 0; i < mid; i++) {
+        prev = tmp;
+        tmp = tmp->next;
+    }
+
+    prev->next = tmp->next;
+    delete tmp;
+    return head;
+}
