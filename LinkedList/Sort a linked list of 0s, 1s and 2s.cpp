0b33128559f4b37532b6289ecd9a78e25aad6164
Shivam Modi~ <smodi9846@gmail.com>
2025-11-28 18:10:53 +0530

    Sort 0 1 2

---
+++ LinkedList/Sort a linked list of 0s, 1s and 2s.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+Node *sort012(Node *head){
+    // Dummy node created first store anything 
+    // just to initialize the next pointers
+    // after travers in LL first for 0s
+    // then for 1s and then for 2s
+    Node *dummy = new Node(-1);
+    Node *ans = dummy;
+    // First traversing for 0s
+    Node *temp = head;
+    while(temp){
+        if(temp->data == 0){
+            dummy->next = new Node(0);
+            dummy = dummy->next;
+        }
+        temp = temp->next;
+    }
+    // Then traversing for 1s
+    temp = head;
+    while(temp){
+        if(temp->data == 1){
+            dummy->next = new Node(1);
+            dummy = dummy->next;
+        }
+        temp = temp->next;
+    }
+    // Finally traversing for 2s
+    temp = head;
+    while(temp){
+        if(temp->data == 2){
+            dummy->next = new Node(2);
+            dummy = dummy->next;
+        }
+        temp = temp->next;
+    }
+    return ans->next;
+    
+}
+
+int main(){
+
+}
