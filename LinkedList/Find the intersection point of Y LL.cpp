dd431dc6944613a2f6e40fdc2c4b3b1f84e6ea64
Shivam Modi~ <smodi9846@gmail.com>
2025-11-28 18:30:12 +0530

    Find the intersection point of Y LL Method2

--- LinkedList/Find the intersection point of Y LL.cpp
+++ LinkedList/Find the intersection point of Y LL.cpp
@@ -12,6 +12,8 @@ class Node{
 };
 
 // method 1 using counting the length of both LLs
+// time complexity: O(N+M)
+// space complexity: O(1)
 Node* getIntersectionNode(Node* headA,Node* headB){
     int countA = 0, countB = 0;
     Node* tempA = headA;
@@ -48,6 +50,38 @@ Node* getIntersectionNode(Node* headA,Node* headB){
         headB = headB->next;
     }
     return NULL;
+}
+
+
+// method 2
+// optimal approach without calculating lengths
+// idea is to traverse both LLs simultaneously
+// when any pointer reaches end of LL, redirect it to head of other LL
+// this will equalize the path length for both pointers
+// they will meet at intersection point or end(NULL) if no intersection exists 
+// time complexity: O(N+M)
+// space complexity: O(1)
+Node* getIntersectionNodeOptimal(Node* A,Node *B){
+    if(!A || !B){
+        return NULL;
+    }
+
+    Node* ptr1 = A;
+    Node* ptr2 = B;
+    while(ptr1 != ptr2){
+        if(!ptr1){
+            ptr1 = B;
+        }else{
+            ptr1 = ptr1->next;
+        }
+
+        if(!ptr2){
+            ptr2 = A;
+        }else{
+            ptr2 = ptr2->next;
+        }
+    }
+    return ptr1;
 }
 
 int main(){}
