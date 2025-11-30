68be8d415ce7d58abf869668dbc0fe0c1e6832f9
Shivam Modi~ <smodi9846@gmail.com>
2025-11-30 21:40:39 +0530

    Add 1 to a number represented by LL

---
+++ LinkedList/Add 1 to a number represented by LL.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+
+Node* addOne(Node* head){
+    
+    // step 1: reverse the LL beacuse addition starts from last digit
+    // step 2: add 1 to the first node and manage carry
+    // step 3: reverse the LL again to restore original order
+    
+}
+
+int main(){
+
+}
