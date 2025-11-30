b6f62d45e210511fdaf3f088988437dd0c7e228a
Shivam Modi~ <smodi9846@gmail.com>
2025-11-30 22:12:23 +0530

    Add 1 to a number represented by LL

--- LinkedList/Add 1 to a number represented by LL.cpp
+++ LinkedList/Add 1 to a number represented by LL.cpp
@@ -1,25 +1,64 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Node{
-    public:
+// Node class representing a single digit in the linked list
+class Node
+{
+public:
     int data;
-    Node* next;
-    Node(int val){
-        data = val;
-        next = NULL;
+    Node *next;
+
+    Node(int value)
+    {
+        data = value;
+        next = nullptr;
     }
 };
 
+// function to reverse the linked list
+Node *reverseList(Node *node)
+{
+    Node *prev = nullptr;
+    Node *current = node;
 
-Node* addOne(Node* head){
-    
-    // step 1: reverse the LL beacuse addition starts from last digit
-    // step 2: add 1 to the first node and manage carry
-    // step 3: reverse the LL again to restore original order
-    
+    while (current)
+    {
+        Node *nextNode = current->next;
+        current->next = prev;
+        prev = current;
+        current = nextNode;
+    }
+    return prev;
 }
 
-int main(){
+// Function to add one to the number represented by the linked list
+Node *addOne(Node *head)
+{
+    // Reverse the list to make least significant digit accessible
+    head = reverseList(head);
 
+    Node *current = head;
+    // Initial carry since we want to add 1
+    int carry = 1;
+
+    // Traverse the list and add carry
+    while (current && carry)
+    {
+        int sum = current->data + carry;
+        current->data = sum % 10;
+        carry = sum / 10;
+
+        // If there's no next node and we still have a carry, append a new node
+        if (!current->next && carry)
+        {
+            current->next = new Node(carry);
+            carry = 0;
+        }
+
+        current = current->next;
+    }
+
+    // Reverse the list back to restore original order
+    head = reverseList(head);
+    return head;
 }
