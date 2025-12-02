47f5a5c8399bcaaee95debe3fb54b7a999c8efa4
Shivam Modi~ <smodi9846@gmail.com>
2025-12-02 22:44:34 +0530

    findPairsWithGivenSum

--- LinkedList/Find pairs with given sum in doubly linked list.cpp
+++ LinkedList/Find pairs with given sum in doubly linked list.cpp
@@ -12,9 +12,40 @@ public:
 };
 
 vector<pair<int, int>> findPairsWithGivenSum(Node *head, int target){
-    
+    vector<int> values;
+    Node* curr = head;
+    while(curr != NULL){
+        values.push_back(curr->data);
+        curr = curr->next;
+    }
+    vector<pair<int,int>> result;
+    // two pointer approach
+    int st=0;
+    int end=values.size()-1;
+    while(st<end){
+        int sum = values[st] + values[end];
+        if(sum == target){
+            // found a pair
+            result.push_back({values[st],values[end]});
+            st++;
+            end--;
+        }else if(sum > target){
+            // need to decrease sum
+            end--;
+        }else{
+            // need to increase sum
+            st++;
+        }
+    }
+    return result;
+
 }
 
 int main(){
 
 }
+
+// Input: head = [1, 2, 4, 5, 6, 8, 9], target = 7
+// Output: [[1, 6], [2, 5]]
+// Explanation:
+// 1 + 6 = 7 and 2 + 5 = 7 are the valid pairs.
