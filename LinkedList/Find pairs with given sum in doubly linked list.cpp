8768cfe8608aa93bf62c01b7d375b1bb223cc486
Shivam Modi~ <smodi9846@gmail.com>
2025-12-02 22:27:42 +0530

    findPairsWithGivenSum

---
+++ LinkedList/Find pairs with given sum in doubly linked list.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next, *prev;
+    Node(int val) : data(val), next(NULL), prev(NULL)
+    {
+    }
+};
+
+vector<pair<int, int>> findPairsWithGivenSum(Node *head, int target){
+    
+}
+
+int main(){
+
+}
