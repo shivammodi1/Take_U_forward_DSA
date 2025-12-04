0935eb129d2da37bd970e3f16d145acee546783f
shivammodi1 <smodi9846@gmail.com>
2025-12-04 22:30:15 +0530

    Rotate LL Right and Left by both solution

--- LinkedList/Rotate a Linked List.cpp
+++ LinkedList/Rotate a Linked List.cpp
@@ -18,6 +18,86 @@ public:
     }
 };
 
+// this for rotating the linked list to the right by k places
+Node* rotate(Node* head, int k){
+    if(!head || k==0){
+        return head;
+    }
+    // compute the length of linked list
+    int count=0;
+    Node*tmp = head;
+    while(tmp){
+        count++;
+        tmp = tmp->next;
+    }
+    k=k%count; // in case k is greater than length of linked list
+    if(k==0){
+        return head;
+    }
+   
+    count = count - k; // to find the new tail position
+    Node* prev= NULL;
+    Node* curr = head;
+    while(count--){
+        prev = curr;
+        curr = curr->next;
+    }
+    //now curr is at new head position
+    // and prev is at new tail position
+    prev->next = NULL; // break the link
+    tmp=curr;
+    while(tmp->next){
+        tmp = tmp->next;
+    }
+    tmp->next = head; // link the old tail to old head
+    return curr; // new head
+}
+
+// this for rotating the linked list to the left by k places
+class Solution {
+  public:
+    Node* rotate(Node* head, int k) {
+        if(!head || k==0){
+            return head;
+        }
+
+        int count=0;
+        Node* tmp = head;
+        while(tmp){
+            count++;
+            tmp = tmp->next;
+        }
+
+        k = k % count;
+        if(k == 0){
+            return head;
+        }
+
+        // FIX: rotate left → break after k nodes
+        count = k;
+
+        Node* prev = NULL;
+        Node* curr = head;
+
+        while(count--){
+            prev = curr;
+            curr = curr->next;
+        }
+
+        prev->next = NULL;
+        tmp = curr;
+
+        while(tmp->next){
+            tmp = tmp->next;
+        }
+
+        tmp->next = head;
+
+        return curr;
+    }
+};
+
+
 int main(){
     return 0;
 }
