f0acddf3d45e3c083914d2756182ce5c126f0a66
shivammodi1 <smodi9846@gmail.com>
2025-12-04 22:15:00 +0530

    Rotate LL by k

---
+++ LinkedList/Rotate a Linked List.cpp
@@ -0,0 +1,23 @@
+// Input: head = [1,2,3,4,5], k = 2
+// Output: [4,5,1,2,3]
+// Explanation:
+// The list is rotated to the right by 2 places.
+// Input: head = [0,1,2], k = 4
+// Output: [2,0,1]
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+int main(){
+    return 0;
+}
