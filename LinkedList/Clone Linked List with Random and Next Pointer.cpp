978114095d21a48724f41b804b01a820683a05e1
shivammodi1 <smodi9846@gmail.com>
2025-12-05 20:23:45 +0530

    Clone List with Next and Random gfg leetcode

---
+++ LinkedList/Clone Linked List with Random and Next Pointer.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+
+// copy linked list with next and random pointer
+
+
+int main(){
+    return 0;
+}
