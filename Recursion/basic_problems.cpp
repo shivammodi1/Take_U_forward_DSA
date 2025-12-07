c2e78e129208494ad314d021e9f2dc9e49a4c72a
shivammodi1 <smodi9846@gmail.com>
2025-12-07 17:05:20 +0530

    Recursion basic concept

--- Recursion/basic_problems.cpp
+++ Recursion/basic_problems.cpp
@@ -11,6 +11,37 @@ void PrintName(int N){
     PrintName(N-1);
 }
 
+// Question 2.
+// Print Nymber from 1 to N
+// Tree structure of function calls for N=3
+// PrintNumbers(3)
+//    |____ PrintNumbers(2)
+//              |
+//              |____ PrintNumbers(1)
+//                        |____ PrintNumbers(0)
+//                                    |
+//                                    return 
+void PrintNumbers(int N){
+    if(N==0){
+        return;
+    }
+    PrintNumbers(N-1);
+    cout<<N<<endl;
+}
+
+// Question 3.
+// Print Number from N to 1
+void PrintNumbersReverse(int N){
+    if(N==0){
+        return;
+    }
+    cout<<N<<endl;
+    PrintNumbersReverse(N-1);
+}
+
 int main(){
     PrintName(5);
+    PrintNumbers(5);
+    PrintNumbersReverse(5);
+    return 0;
 }
