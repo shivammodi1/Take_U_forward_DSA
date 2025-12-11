c90d6b966c2ac3a3ef950acea0d2b2b2035ceebc
shivammodi1 <smodi9846@gmail.com>
2025-12-11 09:23:38 +0530

    fibo recursion tree

--- Recursion/Fibonacci.cpp
+++ Recursion/Fibonacci.cpp
@@ -8,6 +8,16 @@ int Fibo(int n){
     return Fibo(n-1) + Fibo(n-2);
 }
 
+// Recusion Tree
+//                Fibo(4)
+//              /          \
+//        Fibo(3)         Fibo(2)
+//        /    \           /      \
+//    Fibo(2)  Fibo(1)  Fibo(1)    Fibo(0)
+//    /    \
+// Fibo(1) Fibo(0)
+
+
 int main(){
     int n;
     cin>>n;
