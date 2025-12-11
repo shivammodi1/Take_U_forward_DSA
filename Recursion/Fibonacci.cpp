4272d7c3f08cf4b17d43eb9811d5df576d2fe736
shivammodi1 <smodi9846@gmail.com>
2025-12-11 09:21:16 +0530

    fibo solution

--- Recursion/Fibonacci.cpp
+++ Recursion/Fibonacci.cpp
@@ -1,7 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int Fibo(int n){
+    if(n<=1){
+        return n;
+    }
+    return Fibo(n-1) + Fibo(n-2);
+}
 
 int main(){
+    int n;
+    cin>>n;
+    cout<<Fibo(n)<<endl;
     return 0;
 }
