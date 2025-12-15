7dea3c2d1f27ce7f439716cc0d872c4d1de6996d
shivammodi1 <smodi9846@gmail.com>
2025-12-15 21:24:56 +0530

    Subset Sum I tree structure

--- Recursion/Subset Sum I .cpp
+++ Recursion/Subset Sum I .cpp
@@ -16,3 +16,11 @@ int main(){
 // When only 2 is taken then Sum = 2. 
 // When only 3 is taken then Sum = 3. 
 // When elements 2 and 3 are taken then Sum = 2+3 = 5.
+
+// Recursion Tree
+//                 [2 , 3] , sum = 0
+//               /                     \
+//           include 2 , sum = 2        exclude 2 , sum = 0
+//          /            \                 /             \
+//   include 3 , sum=5   exclude 3,sum=2   include 3,sum=3   exclude 3,sum=0
+// Final Sums = [0, 2, 3, 5]
