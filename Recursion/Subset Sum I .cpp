5a5830f8c15e17d0182902bb0b902d127eb67845
shivammodi1 <smodi9846@gmail.com>
2025-12-15 21:22:24 +0530

    Subset Sum I

---
+++ Recursion/Subset Sum I .cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> subsetSums(vector<int>& arr){
+
+}
+
+int main(){
+    return 0;
+}
+
+// Input: arr[] = [2, 3]
+// Output: [0, 2, 3, 5]
+// Explanation: 
+// When no elements are taken then Sum = 0. 
+// When only 2 is taken then Sum = 2. 
+// When only 3 is taken then Sum = 3. 
+// When elements 2 and 3 are taken then Sum = 2+3 = 5.
