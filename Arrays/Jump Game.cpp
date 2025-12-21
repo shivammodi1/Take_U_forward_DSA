51e96a2fe5937af35f8323be5d9783025a43945b
shivammodi1 <smodi9846@gmail.com>
2025-12-21 20:09:04 +0530

    leetcode question can jump

--- Arrays/Jump Game.cpp
+++ Arrays/Jump Game.cpp
@@ -1,6 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// ------------------------------------------------------------------------------------------------------------------------------------------
+// question : 
+//Given an array of non-negative integers nums, you are initially positioned at the first index of the array.
+// Each element in the array represents your maximum jump length at that position.
+// Your goal is to reach the last index in the minimum number of jumps.
+// You can assume that you can always reach the last index.
+// -----------------------------------------------------------------------------------------------------------------------------------------
+// Example:
+// Input: nums = [2,3,1,1,4]
+// Output: true
+// Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+//------------------------------------------------------------------------------------------------------------------------------------------
+// Approach:
+// step 1: set goal to last index
+// step 2: iterate from second last index to first index
+// step 3: if current index + nums[current index] >= goal, update goal to current index
+// step 4: finally check if goal is 0, if yes return true else false
+//------------------------------------------------------------------------------------------------------------------------------------------
+bool canJump(vector<int>& nums){
+
+}
 
 int main(){
     return 0;
