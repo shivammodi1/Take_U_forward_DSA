fe8535582fd0ee113ad30f503f1ee8030f63047d
shivammodi1 <smodi9846@gmail.com>
2026-01-18 20:50:34 +0530

    Check for Balanced Parentheses

---
+++ Stack-Queue/09-Check for Balanced Parentheses.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isBalanced(string& s){
+
+}
+
+int main(){
+    string s;
+    cin>>s;
+    if(isBalanced(s)){
+        cout<<"Balanced"<<endl;
+    }else{
+        cout<<"Not Balanced"<<endl;
+    }
+    return 0;
+}
