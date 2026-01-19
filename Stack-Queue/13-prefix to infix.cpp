e19f183e115173192c7984698f7a258c144b9175
shivammodi1 <smodi9846@gmail.com>
2026-01-19 23:19:55 +0530

    prefix to infix

--- Stack-Queue/13-prefix to infix.cpp
+++ Stack-Queue/13-prefix to infix.cpp
@@ -4,6 +4,25 @@ using namespace std;
 // prefix to infix conversion
 // example: +AB to (A+B)
 
+string preficToInfix(string s)
+{
+    stack<string> st;
+    int n = s.length();
+    int i=n-1;
+    while(i>=0){
+        char ch = s[i];
+        if(ch>='A' && ch<='Z' || ch>='a' && ch<='z' || ch>='0' && ch<='9'){
+            st.push(string(1,ch));
+        }else{
+            string first = st.top(); st.pop();
+            string second = st.top(); st.pop();
+            string ans = "("+first+ch+second+")";
+            st.push(ans);
+        }
+        i--;
+    }
+    return st.top();
+}
 
 int main(){
     return ;
