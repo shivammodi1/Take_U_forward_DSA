89da970b5615799f7e83f63dd712678730055d90
shivammodi1 <smodi9846@gmail.com>
2026-01-21 13:20:04 +0530

    Next Greater Element I

---
+++ Stack-Queue/18-Next Greater Element.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> nextLargerElement(vector<int>& arr){
+    stack<int> s;
+    vector<int> res(arr.size(), -1);
+    int n = arr.size();
+    for(int i=n-1;i>=0;i--){
+        if(s.empty()){
+            s,push(arr[i]);
+        }else{
+            while(!s.empty() && s.top() <= arr[i]){
+                s.pop();
+            }
+            if(!s.empty()){
+                res[i] = s.top();
+            }
+            s.push(arr[i]);
+        }
+    }
+    
+    return res;
+}
+
+int main(){
+    vector<int> arr = {4,5,2,25};
+    vector<int> result = nextLargerElement(arr);
+    for(int val : result){
+        cout << val << " ";
+    }
+    return 0;
+}
