332618ee0fb44350d999468f16a580a72ddb62a2
shivammodi1 <smodi9846@gmail.com>
2026-01-21 10:33:45 +0530

    implement min stack

---
+++ Stack-Queue/17-Implement Min Stack.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// Implement a stack that supports:
+// push(x) -- Push element x onto stack.
+// pop() -- Removes the element on top of the stack.
+// top() -- Get the top element.
+// getMin() -- Retrieve the minimum element in the stack.
+
+
+int main(){
+    return 0;
+}
