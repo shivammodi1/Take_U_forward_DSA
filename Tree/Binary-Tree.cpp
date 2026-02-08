85db5060db3c273325015dc5fc4dba8a0b6175e6
shivammodi1 <smodi9846@gmail.com>
2026-02-08 14:31:05 +0530

    Binary Tree Traversals

--- Tree/Binary-Tree.cpp
+++ Tree/Binary-Tree.cpp
@@ -1,18 +1,57 @@
-#includEW<bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-class TreeNode{
-    public:
+class Node{
+public:
     int data;
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode(int val){
+    Node* left;
+    Node* right;
+
+    Node(int val){
         data = val;
         left = NULL;
         right = NULL;
     }
 };
 
+// function to create a new node until user enters -1
+Node* createNode(){
+    int val;
+    cout<<"Enter value (-1 for NULL): ";
+    cin>>val;
+
+    if(val==-1){
+        return NULL;
+    }
+
+    Node* newNode = new Node(val);
+
+    cout<<"Enter left child of "<<val<<endl;
+    newNode->left = createNode();
+
+    cout<<"Enter right child of "<<val<<endl;
+    newNode->right = createNode();
+
+    return newNode;
+}
+
+// print the tree in pre-order traversal
+void preOrder(Node* root){
+    if(root==NULL){
+        return;
+    }
+
+    cout<<root->data<<" ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+
 int main(){
+
+    Node* root = createNode();
+
+    cout<<"Pre-order traversal of the tree is: "<<endl;
+    preOrder(root);
+
     return 0;
-}98
+}
