0a303f4d6dce98cd0355428f92c822600818bc55
shivammodi1 <smodi9846@gmail.com>
2026-03-01 16:45:35 +0530

    LCA Approach and code

--- Tree/lowest common ancestor.cpp
+++ Tree/lowest common ancestor.cpp
@@ -9,14 +9,27 @@
 // Output: 2
 
 // Approach:-
-// 1. If current node is not null and matches either p or q, return current node.
-// 2. Recursively search for p and q in the left and right subtrees.
-// 3. if both left and right are null, return null.
-// 4. If one of them is null, return the non-null node.
+//1. If the root is NULL, return NULL.
+//2. If the root's data matches either p or q, return the root.
+//3. Recursively search for p and q in the left and right subtrees.
+//4. If both left and right recursive calls return non-NULL, it means p and q are found in different subtrees, so return the current root as the LCA.
+
 
 #include<bits/stdc++.h>
 using namespace std;
 
+
+Node* lowestCommonAncestor(Node* root,int p,int q){
+    if(root==NULL) return NULL;
+    if(root->data==p || root->data==q) return root;
+
+    Node* left=lowestCommonAncestor(root->left,p,q);
+    Node* right=lowestCommonAncestor(root->right,p,q);
+
+    if(left!=NULL && right!=NULL) return root;
+    if(left!=NULL) return left;
+    else return right;
+}
 
 int main(){
     return 0;
