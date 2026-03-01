00908f73be93f4cdbded88d896b97c6ab931bf23
shivammodi1 <smodi9846@gmail.com>
2026-03-01 16:15:32 +0530

    LCA Approach

--- Tree/lowest common ancestor.cpp
+++ Tree/lowest common ancestor.cpp
@@ -1,2 +1,23 @@
 //  Lowest Common Ancestor of a Binary Tree
-// example:- 
+// example:- LCA(4,7)  tree:-    1
+//                             /   \
+//                            2     3
+//                           / \
+//                          4   5
+//                         /   / \
+//                        6   7   8
+// Output: 2
+
+// Approach:-
+// 1. If current node is not null and matches either p or q, return current node.
+// 2. Recursively search for p and q in the left and right subtrees.
+// 3. if both left and right are null, return null.
+// 4. If one of them is null, return the non-null node.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    return 0;
+}
