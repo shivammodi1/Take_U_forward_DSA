299489bd59a433e3da0833ca9eb8c802b7d9631b
shivammodi1 <smodi9846@gmail.com>
2026-03-14 13:02:55 +0530

    Heap

---
+++ Heap/Heap.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+    Heap Data Structure:
+    - It is a complete binary tree which satisfies the heap property.
+    - Each level should be completely filled except possibly the last level, which is filled from left to right.
+    - Stored in an array format where for any given index i:
+        - Left child index = 2*i + 1
+        - Right child index = 2*i + 2
+        - Parent index = (i-1)/2
+
+    Types of Heaps:
+    1. Max Heap:
+        - Every parent node is greater than or equal to its children.
+        - The maximum element is at the root.
+    2. Min Heap:
+        - Every parent node is less than or equal to its children.
+        - The minimum element is at the root.
+    
+    
+*/
+
+int main(){
+    return 0;
+}
