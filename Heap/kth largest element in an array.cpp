61dde39dc07c09abe4f52187c879c80d2a65ce13
shivammodi1 <smodi9846@gmail.com>
2026-03-18 16:35:38 +0530

    Top K Frequent Elements

---
+++ Heap/kth largest element in an array.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// Approach 1: Using Max Heap
+// step1. Build a max heap from the given array.
+// step2. Pop the top element from the max heap k-1 times.
+// step3. The top element of the max heap will be the kth largest element in the array.
+// step4. Return the top element of the max heap as the answer.
+
+// Time Complexity: O(nlogn) + O(klogn) => O(nlogn)
+// Space Complexity: O(n)
+
+int kthLargest(vector<int>& nums,int k){
+    priority_queue<int> pq;
+    for(int i=0;i<nums.size();i++){
+        pq.push(nums[i]);
+    }
+
+    int ans;
+
+    while(!pq.empty() && k--){
+        ans = pq.top();
+        pq.pop();
+    }
+    return ans;
+}
+
+int main(){
+    return 0;
+}
