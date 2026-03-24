c33a00d21853d581e072c4f37f4bb56ee9269f15
shivammodi1 <smodi9846@gmail.com>
2026-03-24 20:46:05 +0530

    Winner of an Election

--- Hasing/Winner of an Election.cpp
+++ Hasing/Winner of an Election.cpp
@@ -14,7 +14,7 @@ class Solution {
     // Function to return the name of candidate that received maximum votes.
     vector<string> winner(string arr[], int n) {
         // Your code here
-        
+                                                                                                                          
         // Return the string containing the name and an integer
         // representing the number of votes the winning candidate got
         unordered_map<string,int>mp;
