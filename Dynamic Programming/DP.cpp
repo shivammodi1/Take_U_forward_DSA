4fa26ff3f6069ddb0c5248c950f6bbefaf10abab
shivammodi1 <smodi9846@gmail.com>
2026-03-30 20:04:34 +0530

    DP start

---
+++ Dynamic Programming/DP.cpp
@@ -0,0 +1,16 @@
+/*
+    # Dynamic Programming (DP):
+    - It is a enhanced version of recursion.
+    - Dp will store the result and reuse it when the same input occurs again.
+    - Jab bhi same input aayega to dp uska result return kar dega.
+
+    # How to identify if a problem can be solved using DP?
+    - 1. Question me choice diya hoga.
+        - Agar question me choice diya hoga to uska matlab hai ki aapke paas do ya usse zyada options honge.
+        - Aapko har option ke liye alag alag result mil sakta hai.
+    - 2. Optimal chize puchege -
+        - Like minimum, maximum, longest, shortest, etc.
+        - Agar question me optimal chize puchege to uska matlab hai ki aapko best solution find karna hai.
+
+*/
+
