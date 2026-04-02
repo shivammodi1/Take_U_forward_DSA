c47d0be1c0c01481b7276ac85dd26ee0863d3be7
shivammodi1 <smodi9846@gmail.com>
2026-04-02 18:42:05 +0530

    Partition Equal Subset Sum

---
+++ Dynamic Programming/0-1 Knapsack Problems/Partition Equal Subset Sum.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+    * Partition Equal Subset Sum
+    * Array is given we need to check:
+    * It is possible to parition the array into two subsets such that the sum of elements in both subsets is same.
+    * Example:
+    * Input: arr[] = {1, 5, 11, 5}
+    * Output: true
+    * Explanation: The array can be partitioned as {1, 5, 5} and {11}
+    
+
+*/
