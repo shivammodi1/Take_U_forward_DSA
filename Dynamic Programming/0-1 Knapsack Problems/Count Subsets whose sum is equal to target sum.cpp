6686c9e8020f4e1dc89df855145bb0d1b062ae17
shivammodi1 <smodi9846@gmail.com>
2026-04-05 15:29:31 +0530

    count subset whose sum is equal to target sum

---
+++ Dynamic Programming/0-1 Knapsack Problems/Count Subsets whose sum is equal to target sum.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+* Count Subsets whose sum is equal to target sum
+
+* Hume 1 array given h and 1 target sum given hai
+* Now hume count krna subset jinka sum target sum ke barabar hai
+* Example: h = [1, 2, 3, 3], target sum = 6
+* Output: 3
+* Explanation: The subsets are [1, 2, 3], [1, 3, 2], [2, 3, 1]
+*/
