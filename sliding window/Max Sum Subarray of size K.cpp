e0fc39ac8117331b19d16099cf10830a27f04d34
shivammodi1 <smodi9846@gmail.com>
2026-04-18 14:13:03 +0530

    Max Sum Subarray of size

--- sliding window/Max Sum Subarray of size K.cpp
+++ sliding window/Max Sum Subarray of size K.cpp
@@ -1,33 +1,61 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
-  public:
-    int maxSubarraySum(vector<int>& arr, int k) {
+// method 1: using sliding window (two pointer approach) TC: O(n) SC: O(1)
+
+int maxSubarraySum(vector<int> &nums, int k)
+{
+    int n = nums.size();
+    int i = 0, j = 0;
+    int sum = 0;
+    int maxSum = INT_MIN;
+
+    while (j < n)
+    {
+        sum += nums[j]; // expand
+
+        // check if window size is k
+        if (j - i + 1 == k)
+        {
+            maxSum = max(maxSum, sum); // calculate
+
+            sum -= nums[i]; // shrink
+            i++;
+        }
+        // agar window size k se chota hai to expand karo
+        j++; // always move j
+    }
+
+    return maxSum;
+}
+
+class Solution
+{
+public:
+    int maxSubarraySum(vector<int> &arr, int k)
+    {
         // code here
-        int n=arr.size();
-        int ans=0;
-        int currSum=0;
-        
+        int n = arr.size();
+        int ans = 0;
+        int currSum = 0;
+
         // current sum till k window
-        for(int i=0;i<k;i++){
-            currSum+=arr[i];
+        for (int i = 0; i < k; i++)
+        {
+            currSum += arr[i];
         }
-        
-        ans=currSum; // max sum 
-        
-        for(int i=k;i<n;i++){
+
+        ans = currSum; // max sum
+
+        for (int i = k; i < n; i++)
+        {
             // from currSum subtract previous element and than add next element
-            currSum = currSum - arr[i-k] + arr[i]; 
-            ans = max(ans,currSum); // store maxSum
+            currSum = currSum - arr[i - k] + arr[i];
+            ans = max(ans, currSum); // store maxSum
         }
-        
+
         return ans;
-        
-        
-        
-        
-        
+
         // for(int i=0;i<n;i++){
         //     int sum=0;
         //   for(int j=i;j<k+i && j<n;j++){
